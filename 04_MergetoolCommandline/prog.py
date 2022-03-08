f363e35b0836a8ac6af61641b4870eed216e86a5
rizvan <s02190101@gse.cs.msu.ru>
2022-03-08 17:14:00 +0300

    Version without completion

---
+++ 04_MergetoolCommandline/prog.py
@@ -0,0 +1,117 @@
+import readline
+import cmd
+import shlex
+import pynames
+import pkgutil
+
+
+class Cool_names_cmd(cmd.Cmd):
+    """Cool commandline!"""
+    intro = "Ryan Gosling. Drive"
+    prompt = "<3 "
+    lang = pynames.LANGUAGE.NATIVE
+    if True:
+        gens = [module_name for _, module_name, _ in pkgutil.iter_modules([pynames.generators.generators_root])]
+        subgens = {gen_name: {subgen_name.replace("NamesGenerator", "").replace("FullnameGenerator", ""): gen
+                              for subgen_name, gen in pynames.generators.__dict__[gen_name].__dict__.items()
+                              if subgen_name.endswith("NamesGenerator") or subgen_name.endswith("FullnameGenerator")}
+                   for gen_name in gens}
+        
+        simple_gens = dict(map(lambda x: x if len(x[1]) == 1 else (x[0], {}), subgens.items()))
+        compl_gens = dict(map(lambda x: x if len(x[1]) > 1 else (x[0], {}), subgens.items()))
+
+
+    def do_language(self, args):
+        """Language switch"""
+        lang = shlex.split(args)
+        if len(lang) != 1:
+            print("Wrong number of arguments")
+        elif lang[0] not in pynames.LANGUAGE.ALL:
+            print(f"The {lang[0]} language is not supported")
+        else:
+            self.lang = lang[0]
+
+
+    def do_generate(self, args):
+        params = shlex.split(args)
+        gender = pynames.GENDER.MALE
+        match len(params):
+            case 3:
+                gen_name, buf, gender = params
+                subclass = self.compl_gens[gen_name] if len(self.simple_gens[gen_name]) == 0 else self.simple_gens[gen_name]
+                gen_class = subclass[buf]
+            case 2:
+                gen_name = params[0]
+                subclass = self.compl_gens[gen_name] if len(self.simple_gens[gen_name]) == 0 else self.simple_gens[gen_name]
+                if params[1].upper() in ["MALE", "FEMALE"]:
+                    gender = params[1].upper()
+                    gen_class = [*subclass.values()][0]
+                else:
+                    gen_class = subclass[params[1]]
+            case 1:
+                gen_name = params[0]
+                subclass = self.compl_gens[gen_name] if len(self.simple_gens[gen_name]) == 0 else self.simple_gens[gen_name]
+                gen_class = [*subclass.values()][0]
+            case _:
+                print("Wrong number of arguments")
+        if gender.upper() == "FEMALE":
+            gender = pynames.GENDER.FEMALE
+        cur_language = self.lang if self.lang in gen_class().languages else pynames.LANGUAGE.NATIVE
+        print(gen_class().get_name_simple(gender, cur_language))
+        # print(gen_class.language)
+
+
+    def do_info(self, args):
+        params = shlex.split(args)
+        
+        if len(params) == 3 or (len(params) == 2 and params[1] not in {"language" ,"female", "male"}):
+            gen_name = params[0]
+            subclass = self.compl_gens[gen_name] if len(self.simple_gens[gen_name]) == 0 else self.simple_gens[gen_name]
+            gen_class = [subclass[params[1]]]
+        else:
+            gen_name = params[0]
+            subclass = self.compl_gens[gen_name] if len(self.simple_gens[gen_name]) == 0 else self.simple_gens[gen_name]
+            gen_class = [*subclass.values()]
+
+        names_sum = 0
+        match params[-1]:
+            case "language":
+                print(*gen_class().languages)
+            case "female":
+                for gen in gen_class:
+                    names_sum += gen().get_names_number(pynames.GENDER.FEMALE)
+                print(names_sum)
+            case "male":
+                for gen in gen_class:
+                    names_sum += gen().get_names_number(pynames.GENDER.MALE)
+                print(names_sum)
+            case _:
+                for gen in gen_class:
+                    names_sum += gen().get_names_number()
+                print(names_sum)
+
+    
+    def do_exit(self, args):
+        """Exit programm"""
+        return True
+
+
+    def do_EOF(self, args):
+        """Exit programm"""
+        return True
+
+
+    def complete_language(self, prefix, allcmd, beg, end):
+        """Completion of the language programm"""
+        return [s for s in pynames.LANGUAGE.ALL if s.startswith(prefix)]
+
+
+    # def complete_info(self, prefix, allcmd, beg, end):
+        # """Completion of info"""
+        # arg_num = len(shlex.split(allcmd))
+        # if arg
+        
+
+
+if __name__ == "__main__":
+    Cool_names_cmd().cmdloop()
